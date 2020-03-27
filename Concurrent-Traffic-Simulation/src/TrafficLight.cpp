e5e5de045725eb378723a87eaf2613457be07a61
root <root@AhmedAliMSoliman530S.localdomain>
2020-03-27 16:10:25 +0400

    FP.4: Implement the method `Send`, which should use the mechanisms `std::lock_guard<std::mutex>` as well as `_condition.notify_one()` to add a new message to the queue and afterwards send a notification. Also, in class `TrafficLight`, create a private member of type `MessageQueue` for messages of type `TrafficLightPhase` and use it within the infinite loop to push each new `TrafficLightPhase` into it by calling send in conjunction with move semantics.

--- Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -18,6 +18,10 @@ void MessageQueue<T>::send(T &&msge)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.push_back(std::move(msge));
+    _condition.notify_one();
 }
 
 
