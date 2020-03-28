171aecaf722337fa3dea66aa7c6bd473f11dfb30
root <root@AhmedAliMSoliman530S.localdomain>
2020-03-28 15:27:38 +0400

    Restructureing the files/dirs for the final submit

---
+++ CourseLessonsSubTasks/L1_Project/build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/src/Graphics.cpp" "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/src/Intersection.cpp" "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/src/Street.cpp" "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/src/TrafficObject.cpp" "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/src/TrafficSimulator-L1.cpp" "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L1.cpp.o"
+  "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/src/Vehicle_Student.cpp" "/root/ahmed/Projects-Mine/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Vehicle_Student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CourseLessonsSubTasks/L2_Project/build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/src/Graphics.cpp" "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/src/Intersection.cpp" "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/src/Street.cpp" "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/src/TrafficObject.cpp" "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/src/TrafficSimulator-L2.cpp" "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L2.cpp.o"
+  "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/src/Vehicle.cpp" "/root/ahmed/MyCppND-Projects/CppND-Concurrent-Traffic-Simulation/L2_Project/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
