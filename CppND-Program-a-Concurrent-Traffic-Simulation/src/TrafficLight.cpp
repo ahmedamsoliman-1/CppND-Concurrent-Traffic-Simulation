da500dc6560e800aa017d95b32bd3edba35e0f13
root <root@AhmedAliMSoliman530S.localdomain>
2020-03-28 14:31:00 +0400

    final project FP.4

--- CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -18,6 +18,10 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+
+    std::lock_guard<std::mutex> lock(mutex);
+    _queue.push_back(std::move(msg))
+    condition.notify_all();
 }
 
 
